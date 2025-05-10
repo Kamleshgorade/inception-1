314d9524b1d62c67b680e809ff230ecfcd512983
kamleshgorade <kamleshgorade1@gmail.com>
2025-05-11 00:50:10 +0530

    update the env

--- my-backend/server.js
+++ my-backend/server.js
@@ -183,25 +183,14 @@ app.use(bodyParser.json());
 app.use('/uploads', express.static('uploads'));
 
 // MySQL configuration
-// const dbConfig = {
-//     host: process.env.MYSQLHOST,
-//     user: process.env.MYSQLUSER,
-//     password: process.env.MYSQLPASSWORD,
-//     database: process.env.MYSQLDATABASE,
-//     //port: process.env.MYSQL_PORT || 3306,
-//     // host: process.env.DB_HOST || 'localhost',
-//     // user: process.env.DB_USER || 'root',
-//     // password: process.env.DB_PASSWORD || 'root',
-//     // database: process.env.DB_NAME || 'master',
-//     // port: process.env.MYSQL_PORT || 3306
-// };
-
-const connection = mysql.createConnection({
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE, // ✅ correct
-  });  
+const dbConfig = {
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || 'root',
+    database: process.env.DB_NAME || 'master',
+    port: process.env.MYSQL_PORT || 3306
+};
+ 
 
 // Upload config
 const storage = multer.diskStorage({
